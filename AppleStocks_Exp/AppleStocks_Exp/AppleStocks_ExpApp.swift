d87c667ac76a03a27dd0619389c9b5767e659583
Shak49 <shakfeizi@gmali.com>
2022-08-08 09:41:32 -0500

    project's files added

---
+++ AppleStocks_Exp/AppleStocks_Exp/AppleStocks_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStocks_ExpApp.swift
+//  AppleStocks_Exp
+//
+//  Created by Shak Feizi on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleStocks_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
